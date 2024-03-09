ce8fb2da9e1480a0cfb95c14eb4c57ad435ff209
deifecommerce <deifecommerce@gmail.com>
2024-03-09 22:10:01 +0200

    new cahnges

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -725,11 +725,8 @@ const quiz_start = async (req, res) => {
     question.answer3 = escapeSpecialCharacters(question.answer3);
     question.answer4 = escapeSpecialCharacters(question.answer4);
 
-    let Questions = quiz.Questions
-    let quizWithoutQuestions = { ...quiz };
-    delete quizWithoutQuestions.Questions;
     
-    res.render("student/quizStart", { title: "Quiz", path: req.path, quiz:quizWithoutQuestions, userData: req.userData, question, Questions,userQuizInfo  });
+    res.render("student/quizStart", { title: "Quiz", path: req.path, quiz, userData: req.userData, question, userQuizInfo });
   } catch (error) {
     res.send(error.message);
   }
